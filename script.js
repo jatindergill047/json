7e7546fcfb9fd030a344ed5e89c0bd7b5587e720
Jatinder Singh <801937@ACDM.DS.SAIT.CA>
2018-10-22 14:26:04 -0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+$('#subBtn').on('click', function(event) {
+    event.preventDefault();
+    var artist = $('#artist').val();
+    var title = $('#songTitle').val();
+    console.log(artist);
+    console.log(title);
+    fetch('https://api.lyrics.ovh/v1/'+artist+'/'+title)
+    
+    .then(function(response) {
+        console.log('Response  is ',response);
+        return response.json();
+    })
+
+.then(function(data) {
+    var ly = document.getElementById('lyrics');
+    ly.innerHTML = data.lyrics;
+    console.log(data);
+})
+});
